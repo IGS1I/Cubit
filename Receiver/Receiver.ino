8c0f96e0d6ed29c2c54ca6df504b67f682496381
erickoala03 <erickchavez083@gmail.com>
2025-09-27 13:53:54 -0400

    Added Receiver as a Access Point.

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -1,5 +1,14 @@
 #include <ESP8266WiFi.h>
 #include <espnow.h>
+const char *ssid = "Miami-Dade"; // The name of the Wi-Fi network that will be created
+const char *password = "Croqueta"; 
+
+
+
+ESP8266WebServer server(80);    // Create a webserver object that listens for HTTP request on port 80
+
+void handleRoot();              // function prototypes for HTTP handlers
+void handleNotFound();
 
 void onReceive(uint8_t *mac, uint8_t *data, uint8_t len) {
   Serial.print("Received from MAC: ");
@@ -25,9 +34,16 @@ void setup() {
   delay(5000);  // Add this delay!
   Serial.println("ESP-NOW Receiver Starting...");
   
-  WiFi.mode(WIFI_STA);
+   //WiFi.mode(WIFI_STA); Orginal one
+  WiFi.mode(WIFI_AP_STA);
+  //WiFi.mode(WIFI_AP);
+  WiFi.softAP("YourSSID", "YourPassword", 6); 
+  
+  //on cinnection sender to reciver 
+  //start on 
   WiFi.disconnect();
- 
+  
+
   if (esp_now_init() != 0) {
     Serial.println("ESP-NOW init failed");
     return;
@@ -38,9 +54,15 @@ void setup() {
  
   Serial.println("ESP-NOW Receiver initialized successfully");
   Serial.println("Waiting for messages...");
+  // WiFi.softAP(ssid, password, 6);
+  // Serial.print("IP address:\t");
+  // Serial.println(WiFi.softAPIP()); 
+  Serial.println("Dual mode initialized");
+  Serial.print("AP IP: ");
+  Serial.println(WiFi.softAPIP());
 }
 
 void loop() {
-  Serial.println("Loop running...");  // Add this to see if loop is working
-  delay(5000);  // Increase delay so it's not too chatty
+  Serial.println("Loop running...");  
+  delay(4000);  // Increase delay so it's not too chatty
 }
