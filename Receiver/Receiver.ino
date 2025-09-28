d8d5ba6446e4b34d95a719d0b86f5dd2605d09c1
erickoala03 <erickchavez083@gmail.com>
2025-09-27 20:53:25 -0400

    git add Receiver\Receiver.ino

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -1,16 +1,26 @@
 #include <ESP8266WiFi.h>
 #include <espnow.h>
+#include <ESP8266WebServer.h>
+
+//Receiever
+
+
 const char *ssid = "Miami-Dade"; // The name of the Wi-Fi network that will be created
 const char *password = "Croqueta"; 
+char last_msg[256] = "(none)";
 
-
-
-ESP8266WebServer server(80);    // Create a webserver object that listens for HTTP request on port 80
+uint8_t last_len = 0;
+uint32_t last_msg_id = 0;  
 
 void handleRoot();              // function prototypes for HTTP handlers
 void handleNotFound();
 
+  // Create a webserver object that listens for HTTP request on port 80
+ESP8266WebServer server(80);
+
+
 void onReceive(uint8_t *mac, uint8_t *data, uint8_t len) {
+
   Serial.print("Received from MAC: ");
   for (int i = 0; i < 6; i++) {
     Serial.printf("%02X", mac[i]);
@@ -22,11 +32,25 @@ void onReceive(uint8_t *mac, uint8_t *data, uint8_t len) {
   for (int i = 0; i < len; i++) {
     Serial.print((char)data[i]);
   }
+
+  size_t n = (len < sizeof(data) - 1) ? len : (sizeof(last_msg) - 1);
+  memcpy(last_msg, data, n); 
+  last_msg[n] = '\0';          // make sure it's null-terminated
+  last_len = n;
   Serial.println();
  
   Serial.print("Length: ");
   Serial.println(len);
+  
+ 
+  esp_now_send(mac, (uint8_t*)&ack, sizeof(ack));
+
+                 // Actually start the server
+  Serial.println("HTTP server started");
+  Serial.print("AP IP: ");
+  Serial.println(WiFi.softAPIP());
   Serial.println("---");
+
 }
 
 void setup() {
@@ -34,14 +58,12 @@ void setup() {
   delay(5000);  // Add this delay!
   Serial.println("ESP-NOW Receiver Starting...");
   
-   //WiFi.mode(WIFI_STA); Orginal one
   WiFi.mode(WIFI_AP_STA);
-  //WiFi.mode(WIFI_AP);
-  WiFi.softAP("YourSSID", "YourPassword", 6); 
   
-  //on cinnection sender to reciver 
+  WiFi.softAP(ssid, password, 6); // 6 is the same channel for receiver 
+
+  
   //start on 
-  WiFi.disconnect();
   
 
   if (esp_now_init() != 0) {
@@ -54,15 +76,47 @@ void setup() {
  
   Serial.println("ESP-NOW Receiver initialized successfully");
   Serial.println("Waiting for messages...");
-  // WiFi.softAP(ssid, password, 6);
-  // Serial.print("IP address:\t");
-  // Serial.println(WiFi.softAPIP()); 
+  
   Serial.println("Dual mode initialized");
   Serial.print("AP IP: ");
   Serial.println(WiFi.softAPIP());
+  server.on("/", handleRoot);  // Register the root handler
+  server.onNotFound(handleNotFound);  
+  server.begin();             // Start the server
 }
 
 void loop() {
-  Serial.println("Loop running...");  
-  delay(4000);  // Increase delay so it's not too chatty
+
+  server.handleClient(); 
+  static unsigned long lastPrint = 0;
+  if (millis() - lastPrint > 10000) {  // Every 10 seconds
+    Serial.println("Server running... waiting for connections");
+    lastPrint = millis();
+  }
+  
+  delay(10); // Increase delay so it's not too chatty
+}
+
+void handleRoot() {
+
+  String html = "<!DOCTYPE html>";
+  html += "<html><head>";
+  html += "<meta name='viewport' content='width=device-width, initial-scale=1.0'>";
+  html += "<title>ESP8266 Server</title>";
+  html += "</head><body>";
+  html += "<h1>Hello from ESP8266!</h1>";
+  html += "<p>Web server is working!</p>";
+  html += "<p>";
+  html += last_msg;
+  html += "</p>";
+  html += "<p>ESP-NOW receiver is active</p>";
+  html += "<p>IP: 192.168.4.1</p>";
+  html += "</body></html>";
+  
+  server.send(200, "text/html", html);
+  Serial.println("Homepage served to client");  // Send HTTP status 200 (Ok) and send some text to the browser/client
+}
+
+void handleNotFound(){
+  server.send(404, "text/plain", "404: Not found"); // Send HTTP status 404 (Not Found) when there's no handler for the URI in the request
 }
