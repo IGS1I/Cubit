865c52e6ed209d61446acf10a652db8bd8f341e8
William K. Coleman <168572788+IGS1I@users.noreply.github.com>
2025-09-28 03:32:01 -0400

    edit to test

--- receive/receive.ino
+++ receive/receive.ino
@@ -1,6 +1,6 @@
 #include <SoftwareSerial.h>
 // RX, TX (Arduino’s perspective)
-SoftwareSerial esp(10, 11); // pin 10 <- ESP TX, pin 11 -> ESP RX (unused if one-way)
+SoftwareSerial esp(0, 1); // pin 10 <- ESP TX, pin 11 -> ESP RX (unused if one-way)
 
 void setup() {
   Serial.begin(115200);   // for PC Serial Monitor
